37dad2d760aecd32a2d7c9df95bd90c8fe0716d5
Gabriel Oliveira <gabrielviol00@hotmail.com>
2024-01-18 10:54:34 -0300

    [WIP]

--- src/pages/api/postDriver.api.ts
+++ src/pages/api/postDriver.api.ts
@@ -2,29 +2,29 @@ import { prisma } from "@/lib/prisma";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    return res.status(405).end()
-  }
+   if (req.method !== 'POST') {
+      return res.status(405).end()
+   }
 
-  const { name, id } = req.body
+   const { name, id } = req.body
 
-  const driverExist = await prisma.driver.findUnique({
-    where: {
-      name
-    }
-  })
+   const driverExist = await prisma.driver.findUnique({
+      where: {
+         name
+      }
+   })
 
-  if (driverExist) {
-    return res.status(400).json({
-      message: 'Name already taken.'
-    })
-  }
+   if (driverExist) {
+      return res.status(400).json({
+         message: 'Name already taken.'
+      })
+   }
 
-  const driver = await prisma.driver.create({
-    data: {
-      name,
-      id
-    }
-  })
-  return res.status(201).json(driver)
+   const driver = await prisma.driver.create({
+      data: {
+         name,
+         id
+      }
+   })
+   return res.status(201).json(driver)
 }
