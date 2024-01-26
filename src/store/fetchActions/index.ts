00e39b332e865da190152ff6e67b72a0eb9debfd
Gabriel Oliveira <gabrielviol00@hotmail.com>
2024-01-26 17:41:01 -0300

    [WIP]

--- src/store/fetchActions/index.ts
+++ src/store/fetchActions/index.ts
@@ -3,21 +3,21 @@ import { setCarpool, setDrivers } from "../reducers/driverReducer"
 import { Dispatch } from "redux"
 
 export const fetchDrivers = () => async (dispatch: Dispatch) => {
-  try {
-    const response = await api.get('/getDrivers')
-    const drivers = response.data
-    dispatch(setDrivers(drivers))
-  } catch (error) {
-    console.log(error)
-  }
+   try {
+      const response = await api.get('/getDrivers')
+      const drivers = response.data
+      dispatch(setDrivers(drivers))
+   } catch (error) {
+      console.log(error)
+   }
 }
 
 export const fetchCarpool = () => async (dispatch: Dispatch) => {
-  try {
-    const response = await api.get('/getCarpool')
-    const carpool = response.data
-    dispatch(setCarpool(carpool))
-  } catch (err) {
-    console.error(err)
-  }
+   try {
+      const response = await api.get('/getCarpool')
+      const carpool = response.data
+      dispatch(setCarpool(carpool))
+   } catch (err) {
+      console.error(err)
+   }
 }
